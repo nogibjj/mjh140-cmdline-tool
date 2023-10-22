f46bc5172be7f2e6e2e0b605754d97fe9a9b4e41
Matthew Holden <matthew.holden86@gmail.com>
2023-10-22 13:55:52 -0400

    clippy

--- src/main.py
+++
@@ -1,9 +0,0 @@
-'''Print Module'''
-
-def printfunc(newstr : str) -> str:
-    '''Print function.'''
-
-    return print(newstr)
-
-if __name__ == "__main__":
-    printfunc("Hello World!")
--- test_redir.py
+++
@@ -1,7 +0,0 @@
-import os, pathlib
-import pytest
-cwd = os.getcwd()
-path = cwd + '/tests/'
-os.chdir(path)
-#print(pathlib.Path.cwd())
-pytest.main()
